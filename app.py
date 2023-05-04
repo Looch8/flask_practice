7733f4e7f1af27ade9bae1816e305739a98e48e2
Looch8 <lukewheldale@gmail.com>
2023-05-04 19:41:18 +0930

    practiced creating my own API

--- app.py
+++ app.py
@@ -1,18 +1,79 @@
 from flask import Flask
+import json
+from datetime import datetime
+import random
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def homepage():
+    return """<p>Hi, welcome to my API! Here are the endpoints that are available:</p>
+            <ul>
+                <li>Current Time: /time</li>
+                <li>Educator Info: /educators</li>
+            </ul> """
 
 
-@app.route("/goodbye/")
-def goodbye_world():
-    return "<p>Goodbye, World!</p>"
+@app.route('/time/')
+def current_time():
+    time_dict = {"current time": str(datetime.now().strftime('%H:%M'))}
+    return json.dumps(time_dict)
 
 
-@app.route("/coder/")
-def coder():
-    return "<p>This web app was created in a class at Coder Academy</p>"
+@app.route('/educators/')
+def educators():
+    educator_dict = {
+        "educators": [
+            {
+                "Name": "Oliver",
+                "Specialty": "Automated testing"
+            },
+            {
+                "Name": "Jairo",
+                "Specialty": "Discrete Mathematics"
+            },
+            {
+                "Name": "Amir",
+                "Specialty": "Web Development"
+            },
+            {
+                "Name": "Iryna",
+                "Specialty": "Database Engineering"
+            },
+            {
+                "Name": "George",
+                "Specialty": "Internet Security"
+            },
+        ]
+    }
+    return json.dumps(educator_dict)
+
+
+@app.route('/coinflip/')
+def coinflip():
+    choices = ['heads', 'tails']
+    result = random.choice(choices)
+    coin_dict = {
+        "result": result
+    }
+    return json.dumps(coin_dict)
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
+
+# @app.route("/goodbye/")
+# def goodbye_world():
+#     return "<p>Goodbye, World!</p>"
+
+
+# @app.route("/coder/")
+# def coder():
+#     return "<p>This web app was created in a class at Coder Academy</p>"
+
+
+# @app.route("/current_time/")
+# def current_time():
+#     date = datetime.now()
+#     return str(date)
